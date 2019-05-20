cff9edb18a95f1a9203cccd68ce7186ad7f57054
DinMon <dinysmon2013@gmail.com>
2019-05-20 15:09:51 +1000

    init iOSMovieApp Xcode project

---
+++ iOSMovieApp/iOSMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSMovieApp
+//
+//  Created by GUILLAUME DINYS MONVOISIN on 20/5/19.
+//  Copyright © 2019 GUILLAUME DINYS MONVOISIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
